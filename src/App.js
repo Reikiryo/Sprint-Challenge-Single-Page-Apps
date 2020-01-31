f44534fefbb179159a11c3c1b5487fb19a2d185e
Josiah <reikiryo@yahoo.com>
2020-01-31 11:20:56 -0500

    Ads Routes for home and character pages, as well as Navlinks to get between them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import { Route, NavLink } from "react-router-dom"
+import WelcomePage from './components/WelcomePage'
+import CharacterList from './components/CharacterList'
 
 export default function App() {
   return (
     <main>
+      <NavLink to="/">Home</NavLink>
       <Header />
+      <NavLink to="/characters">Characters</NavLink>
+
+      <Route exact path="/">
+        <WelcomePage/>
+      </Route>
+
+      <Route path="/characters">
+        <CharacterList/>
+      </Route>
+      
     </main>
   );
 }
